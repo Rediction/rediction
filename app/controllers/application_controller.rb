838753a214bbf9b87e749953ef764d71391d0923
APPLE4869 <apple.4869.s@gmail.com>
2018-07-03 23:53:09 +0900

    Basic認証を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_authentication
+
+  private
+
+    # Basic認証
+    def basic_authentication
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'rediction' && password == 'dictionary'
+      end
+    end
 end
