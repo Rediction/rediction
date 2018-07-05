c564c26f5ae5e0f88a7a31eb934021110a7f5bbd
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-07-05 16:48:39 +0900

    corrected

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
 
-  validates :email, {presence: true}
-  validates :password, {presence: true}
+  has_secure_password validations: true
+
+  validates :email,    presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true
 end
--- app/models/user_changes.rb
+++ app/models/user_changes.rb
@@ -1,6 +1,21 @@
-class User < ApplicationRecord
+# == Schema Information
+#
+# Table name: user_changes
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  email      :bigint(8)
+#  password   :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 
-  validates :user_id,  {presence: true}
-  validates :email,    {presence: true}
-  validates :password, {presence: true}
+class UserChanges < ApplicationRecord
+
+  has_secure_password validations: true
+  belongs_to :user, foreign_key: 'user_id'
+
+  validates :user_id,  presence: true
+  validates :email,    presence: true
+  validates :password, presence: true
 end
