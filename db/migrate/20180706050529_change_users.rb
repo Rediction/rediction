231394ab15b073fc4101a2f1b8ae9dc26b295bde
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-07-06 16:54:53 +0900

    schema.rbを変更しました

--- db/migrate/20180706050529_change_users.rb
+++ db/migrate/20180706050529_change_users.rb
@@ -1,9 +1,7 @@
 class ChangeUsers < ActiveRecord::Migration[5.2]
   def change
-    add_index :user_changes, :user_id
-
     create_table :user_changes do |t|
-      t.bigint    :user_id,          null: false, comment: "更新したユーザーのid"
+      t.bigint    :user_id,          null: false, index:true, comment: "更新したユーザーのid"
       t.string    :email,            null: false, comment: "更新したメールアドレス"
       t.string    :password_digest,  null: false, comment: "更新されたパスワード"
       t.string    :event,            null: false, comment: "レコード登録時のイベント"
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_07_06_032146) do
-
+ActiveRecord::Schema.define(version: 2018_07_06_050529) do
 
   create_table "user_changes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false, comment: "更新したユーザーのid"
+    t.string "email", null: false, comment: "更新したメールアドレス"
+    t.string "password_digest", null: false, comment: "更新されたパスワード"
+    t.string "event", null: false, comment: "レコード登録時のイベント"
+    t.datetime "created_at", comment: "ユーザー情報が更新された日時"
+    t.index ["user_id"], name: "index_user_changes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "email", null: false, comment: "メールアドレス"
+    t.string "password_digest", null: false, comment: "パスワード"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
