94eda063685767af9a6f387b071ca05ecb3ca3e1
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-07-28 16:41:18 +0900

    会員登録完了までの実装/user_profiles_controllerに処理を記述

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,9 +1,23 @@
 class UserProfilesController < ApplicationController
+  before_action :get_provisional_user, only: [:new]
+
   def new
     @user_profile = UserProfile.new
+    @user = User.new(email: @provisional_user.email, password: @provisional_user.password_digest)
+    @user.save
+    @user_auth_log = UserAuthLog.new(user_id: @user.id, success: true)
+    @user_auth_log.save
+    @provisional_user_completed_log = ProvisionalUserCompletedLog.new(user_id: @user.id, provisional_user_id: @provisional_user.id)
+    @provisional_user_completed_log.save
+    @user_change = UserChange.create_from_original!(original_record: @user, event: 'create')
+    @user_change.save
   end
 
   def create
-    
+
+  end
+
+  def get_provisional_user
+    @provisional_user = ProvisionalUser.find_by(verification_token: params[:verification_token])
   end
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -20,4 +20,5 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   validates :password_digest, presence: true
+
 end
