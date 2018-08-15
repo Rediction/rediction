7d5b872e1d21a792c2681cf7e4cc4182fa47f252
中嶋健太 <akahaya719-n.k@i.softbank.jp>
2018-08-15 16:17:18 +0900

    update

--- app/models/favorite_change.rb
+++ app/models/favorite_change.rb
@@ -3,8 +3,8 @@
 # Table name: favorite_changes # お気に入りが外された時の履歴
 #
 #  id         :bigint(8)        not null, primary key
-#  user_id    :bigint(8)        not null              # ユーザーID(FK)
-#  word_id    :bigint(8)        not null              # 言葉ID(FK)
+#  user_id    :bigint(8)        not null              # ユーザーID
+#  word_id    :bigint(8)        not null              # 言葉ID
 #  event      :string(255)      not null              # イベント
 #  created_at :datetime         not null
 #
--- app/models/word_change.rb
+++ app/models/word_change.rb
@@ -3,8 +3,8 @@
 # Table name: word_changes # 投稿が削除された時の履歴
 #
 #  id          :bigint(8)        not null, primary key
-#  user_id     :bigint(8)        not null              # ユーザーID(FK)
-#  word_id     :bigint(8)        not null              # 言葉ID(FK)
+#  user_id     :bigint(8)        not null              # ユーザーID
+#  word_id     :bigint(8)        not null              # 言葉ID
 #  name        :string(255)      not null              # 名前
 #  phonetic    :string(255)      not null              # ふりがな
 #  description :string(255)      not null              # 説明
