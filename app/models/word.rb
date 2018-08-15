d7e60ab4cdc75a7d056c2979f6c7795a66b9c5c7
APPLE4869 <apple.4869.s@gmail.com>
2018-08-16 00:28:46 +0900

    WordクラスでPerformableWithChangesをincludeするように修正

--- app/models/word.rb
+++ app/models/word.rb
@@ -11,24 +11,12 @@
 #
 
 class Word < ApplicationRecord
+  include PerformableWithChanges
+
   belongs_to :user
 
   validates :user_id, presence: true
   validates :name, presence: true
   validates :phonetic, presence: true
   validates :description, presence: true
-
-  def save_with_changes!
-    ActiveRecord::Base.transaction do
-      save!
-      WordChange.create_from_original!(original_record: self, event: "create")
-    end
-  end
-
-  def destroy_with_changes!
-    ActiveRecord::Base.transaction do
-      WordChange.create_from_original!(original_record: self, event: "delete")
-      destroy!
-    end
-  end
 end
