558589c84824d359de64875b632672abcb13f25b
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-08-17 01:13:08 +0900

    指摘修正⑨

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -7,7 +7,8 @@ class UserProfilesController < ApplicationController
 
   def create
     # user_profilesテーブルと_changesテーブルにデータを格納
-    @user_profile = current_user.build_profile(user_profile_params)
+    user_profile = current_user.build_profile(user_profile_params)
+    user_profile.save_with_changes!
 
     # TODO(shuji ota):画面の遷移先をタイムラインの画面に変更する
     redirect_to root_path, flash: { success: "プロフィール登録が完了しました。" }
