07b3cbf0c7585000e60a90682300d32d7764b8d1
APPLE4869 <apple.4869.s@gmail.com>
2018-08-20 01:07:59 +0900

    ダッシュボードに言葉の総数とお気に入り総数を表示

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,10 +1,7 @@
 class Admin::DashboardsController < Admin::ApplicationController
   def show
     @users_count = ::User.count
-
-    # TODO(Shokei Takanashi)
-    # WordとFavoriteそれぞれを実装した後に、コメントアウトを外して、それぞれの総数を表示するように改修する
-    # @words_count = ::Word.count
-    # @favorites_count = ::Favorite.count
+    @words_count = ::Word.count
+    @favorites_count = ::Favorite.count
   end
 end
