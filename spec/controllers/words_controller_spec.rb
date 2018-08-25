ca29b64a8c0f67b9d1d709fadb29db7c994d44b5
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-08-25 12:37:46 +0900

    words_controllerのテスト（delete以外）

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -74,4 +74,70 @@ describe WordsController, type: :controller do
       end
     end
   end
+
+  describe "GET #show" do
+    subject { get :show, params: { id: word.id } }
+    before { subject }
+
+    context "平常時アクセスの場合" do
+      let(:word) { create(:word) }
+      it "HTTP 200 OK", :aggregate_failures do
+        expect(response).to have_http_status 200
+        expect(response).to render_template :show
+      end
+    end
+  end
+
+  describe "POST #create" do
+    let(:word_params) { attributes_for(:word) }
+
+    context "@wordの保存に成功した場合" do
+      subject{ post :create, params: { word: word_params } }
+
+      it "レコードが生成されること", :aggregate_failures do
+        expect{ subject }.to change(Word, :count).by(1)
+        expect(response).to have_http_status 302
+        expect(flash[:success]).to eq "投稿しました。"
+        expect(response).to redirect_to user_mypage_path
+      end
+    end
+
+    context "@wordの保存に失敗した場合" do
+      subject{ post :create, params: { word: word_params.merge(description: description) } }
+      let(:description) { "" }
+
+      it "レコードが生成されず、newテンプレートにrenderすること", :aggregate_failures do
+        expect{ subject }.to change(Word, :count).by(0)
+        expect(flash[:error]).to eq "投稿に失敗しました。"
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
