5c34ae7429a87b5a28a6e3a92b1cfc4ca9de2622
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-08-27 17:43:29 +0900

    パスワード編集機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,8 +35,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, email: true
 
   # TODO(shuji ota):形式チェックのvalidationを追加する
-  validates :password, length: (8..32), confirmation: true, unless: :password_digest
-  validates :password_confirmation, presence: true
+  validates :password, length: (8..32), confirmation: true
+  validates :password_confirmation, presence: true, only: :update
 
   # 稼働中のアカウント(未凍結 & 退会していない)
   scope :active, -> { unfreezed.unresigned }
