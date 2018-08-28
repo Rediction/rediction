203925018802ef0999b9f13fa5405fb4344bc54f
APPLE4869 <apple.4869.s@gmail.com>
2018-08-29 01:16:11 +0900

    Word一覧のHeaderのJS処理を修正

--- app/javascript/packs/words/_index_nav.ts
+++ app/javascript/packs/words/_index_nav.ts
@@ -1,22 +1,15 @@
 import $ from "jquery";
 
 // Word一覧切り替え用メニューの表示・非表示イベントを設定
-export const setWordChangeModeMenuAction = () => {
-  $(document).on(
-    "click",
-    "#word-change-mode-menu-btn",
-    toggleWordChangeModeMenu
-  );
+export const setWordChangeModeMenuAction = function() {
+  $(document).on("click", "#word-change-mode-menu-btn", function() {
+    $("#word-change-mode-menu").toggleClass("active");
+  });
 
-  $(document).on("click", "#word-change-mode-menu", e => {
+  $(document).on("click", "#word-change-mode-menu", function(e) {
     if ($(e.target).closest("a").length === 1) {
       return;
     }
-    toggleWordChangeModeMenu();
+    $("#word-change-mode-menu").toggleClass("active");
   });
-};
-
-// word-change-mode-menuの表示・非表示を切り替え
-const toggleWordChangeModeMenu = () => {
-  $("#word-change-mode-menu").toggleClass("active");
 };
