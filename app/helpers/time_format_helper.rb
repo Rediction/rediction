d13d58c95fd00df66e111bc119da77e8ed62c85b
APPLE4869 <apple.4869.s@gmail.com>
2018-08-29 01:40:13 +0900

    時間表記を修正

--- app/helpers/time_format_helper.rb
+++ app/helpers/time_format_helper.rb
@@ -1,12 +1,12 @@
 module TimeFormatHelper
   # 日時(datetime型)を全て日本語名に変換
   def ja_full_datetime(datetime)
-    datetime.strftime("%Y年%m月%d日 %H時間%M分%S秒")
+    datetime.strftime("%Y年%m月%d日 %H時%M分%S秒")
   end
 
   # 日時(datetime型)を分単位まで日本語名に変換
   def ja_middle_datetime(datetime)
-    datetime.strftime("%Y年%m月%d日 %H時間%M分")
+    datetime.strftime("%Y年%m月%d日 %H時%M分")
   end
 
   # 日付(date型)を全て日本語名に変換
