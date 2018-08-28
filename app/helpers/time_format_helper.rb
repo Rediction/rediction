96b906cd1573967e17ad4fda22fcb863de259a11
APPLE4869 <apple.4869.s@gmail.com>
2018-08-29 01:02:35 +0900

    Word詳細の投稿日時のフォーマットを修正

--- app/helpers/time_format_helper.rb
+++ app/helpers/time_format_helper.rb
@@ -4,6 +4,11 @@ module TimeFormatHelper
     datetime.strftime("%Y年%m月%d日 %H時間%M分%S秒")
   end
 
+  # 日時(datetime型)を分単位まで日本語名に変換
+  def ja_middle_datetime(datetime)
+    datetime.strftime("%Y年%m月%d日 %H時間%M分")
+  end
+
   # 日付(date型)を全て日本語名に変換
   def ja_full_date(date)
     date.strftime("%Y年%m月%d日")
