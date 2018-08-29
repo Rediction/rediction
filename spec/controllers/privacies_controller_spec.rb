9db915aa0efcacc53ca4022f17f37d07871edb38
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-08-29 15:17:50 +0900

    テスト

---
+++ spec/controllers/privacies_controller_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe PrivaciesController, type: :controller do
+  include_context "current_userとしてログイン後にアクセスする"
+
+  describe "GET #show" do
+    subject { get :show }
+    before { subject }
+
+    context "平常時アクセスの場合", type: :doing do
+      it "HTTP 200 OK", :aggregate_failures do
+        expect(response).to have_http_status 200
+        expect(response).to render_template :show
+      end
+    end
+  end
+end
