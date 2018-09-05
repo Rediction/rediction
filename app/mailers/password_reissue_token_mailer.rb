e0457a943da103a519fd0f5ac509889f02ac1a66
APPLE4869 <apple.4869.s@gmail.com>
2018-09-06 08:53:08 +0900

    titleを設定

--- app/mailers/password_reissue_token_mailer.rb
+++ app/mailers/password_reissue_token_mailer.rb
@@ -2,6 +2,6 @@ class PasswordReissueTokenMailer < ApplicationMailer
   # 本会員登録の認証用メールを送信する処理
   def send_url_to_reissue(password_reissue_token)
     @password_reissue_token = password_reissue_token
-    mail to: @password_reissue_token.email, subject: "メール認証"
+    mail to: @password_reissue_token.email, subject: "パスワード再発行 | Rediction"
   end
 end
