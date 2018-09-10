9c772d9e9ef65a8b2ea4d6b240cb40eb35a369ff
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-09-10 18:32:14 +0900

    passwordの形式チェックのvalidationを追加

--- app/validators/password_validator.rb
+++ app/validators/password_validator.rb
@@ -1,10 +1,9 @@
 class PasswordValidator < ActiveModel::EachValidator
-  SYMBOL = '!@#$%?\/+\-'
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#{SYMBOL}])[a-zA-Z0-9#{SYMBOL}]+\z/
+  PASSWORD_SYMBOL = '!@#$%?\/+\-'
+  VALID_PASSWORD_REGEX =
+    /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#{PASSWORD_SYMBOL}])[a-zA-Z0-9#{PASSWORD_SYMBOL}]{8,32}\z/
 
   def validate_each(record, attribute, value)
-    return record.errors.add(attribute, "の形式が正しくありません。") if value.match(VALID_PASSWORD_REGEX).blank?
-
-    record.errors.add(attribute, "の長さは8文字以上32文字以内です。") if value.length < 8 || value.length > 32
+    record.errors.add(attribute, "の形式が正しくありません。") unless value =~ VALID_PASSWORD_REGEX
   end
 end
