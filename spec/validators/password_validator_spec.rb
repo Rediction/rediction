86f90ead89f246aa925c64e6dc6d7657905f2246
shuji ota <kerokerogeorge@yahoo.co.jp>
2018-09-11 11:21:42 +0900

    passwordの形式チェックのvalidationを追加

--- spec/validators/password_validator_spec.rb
+++ spec/validators/password_validator_spec.rb
@@ -4,50 +4,43 @@ describe PasswordValidator, type: :validator do
   let(:klass) do
     Struct.new(:password) do
       include ActiveModel::Validations
-
-      def self.name
-        "PasswordValidator"
-      end
-
-      validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%?\/+\-])[a-zA-Z0-9!@#$%?\/+\-]+\z/}
+      validates :password, password: true
     end
   end
 
-  describe "#validate_each", type: :doing do
+  describe "#validate_each" do
     subject { klass.new(password) }
 
     context "大文字アルファベットを含んでいない場合" do
       let(:password) { "password1!" }
 
-      it { is_expected.to_not be_valid }
+      it { is_expected.to be_invalid }
     end
 
     context "小文字アルファベットを含んでいない場合" do
       let(:password) { "PASSWORD1!" }
 
-      it { is_expected.to_not be_valid }
+      it { is_expected.to be_invalid }
     end
 
     context "数字を含んでいない場合" do
       let(:password) { "Password!" }
 
-      it { is_expected.to_not be_valid }
+      it { is_expected.to be_invalid }
     end
 
     context "記号を含んでいない場合" do
       let(:password) { "Password1" }
 
-      it { is_expected.to_not be_valid }
+      it { is_expected.to be_invalid }
     end
-  end
 
-  describe "success", type: :doing do
-    subject { klass.new(password) }
+    describe "success" do
+      context "半角大文字英字、半角小文字英字、半角数字、記号の４つ全てが含まれている場合" do
+        let(:password) { "Password1!" }
 
-    context "全て含まれている場合" do
-      let(:password) { "Password1!" }
-
-      it { is_expected.to be_valid }
+        it { is_expected.to be_valid }
+      end
     end
   end
 end
